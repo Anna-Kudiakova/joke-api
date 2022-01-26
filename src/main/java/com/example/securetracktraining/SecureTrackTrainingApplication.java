aab40d47e5b57ddbd641f878b4df26c3d3b4b851
Anna Kudiakova <anna.kudiakova@eleks.com>
2022-01-26 23:26:23 +0200

    Initial project skeleton set

---
+++ src/main/java/com/example/securetracktraining/SecureTrackTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.example.securetracktraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTrackTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTrackTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securetracktraining/SecureTrackTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securetracktraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTrackTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
